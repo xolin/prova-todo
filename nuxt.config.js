2bcc5789557ab69c9ad83ed53fb71e4fce29f0e2
Joan <joanetap@gmail.com>
2021-11-15 18:32:17 +0100

    Add Lodash

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,6 +23,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    { src: '~/plugins/lodash.js' }
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
@@ -31,7 +32,7 @@ export default {
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
+    '@nuxt/typescript-build'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
